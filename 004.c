770c9ecd33ce4e49eebe736aedeac4b445d5db1c
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-12-25 00:12:29 +0800

    005

---
+++ 004.c
@@ -0,0 +1,14 @@
+/*为两个整数赋整数值并显示*/
+#include<stdio.h>
+int main(void){
+
+    int vx,vy;
+
+    vx=57;
+    vy=vx+10;
+
+    printf("vx的值是%d。\n",vx);
+    printf("vy的值是%d。\n",vy);
+
+    return 0;
+}
