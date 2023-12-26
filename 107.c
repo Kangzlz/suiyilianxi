e72c27ef715b03dd7f3318306636b8a2388e6e9a
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-12-26 21:32:40 +0800

    108

---
+++ 107.c
@@ -0,0 +1,17 @@
+/*读取两个实数值，用实数显示它们的和、差、积、商*/
+#include<stdio.h>
+int main(void)
+{
+    double vx,vy;
+
+    puts("请输入两个实数。");
+    printf("实数vx:"); scanf("%lf",&vx);
+    printf("实数vy:"); scanf("%lf",&vy);
+
+    printf("vx+vy=%f\n",vx+vy);
+    printf("vx-vy=%f\n",vx-vy);
+    printf("vx*vy=%f\n",vx*vy);
+    printf("vx/vy=%f\n",vx/vy);
+
+    return 0;
+}
