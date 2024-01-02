400da47cb4bf681ec14015ede268eec8d32e3049
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2024-01-02 13:29:00 +0800

    410

---
+++ lianxi3/405.c
@@ -0,0 +1,17 @@
+/*从输入的整数开始倒数到零*/
+#include<stdio.h>
+int main(void)
+{
+    int no;
+
+    printf("请输入一个整数:");
+    scanf("%d",&no);
+
+    while(no>=0){
+        printf("%d ",no);
+        no--;
+    }
+    putchar('\n');
+
+    return 0;
+}
