fadc10ac95978e01ad0e0b35201b91463f6a0c8f
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2024-01-13 12:12:21 +0800

    707

--- lianxi3/706.c
+++ lianxi3/706.c
@@ -26,8 +26,8 @@ int main(void)
 {
     unsigned a,b;
     printf("请输入两个非负整数。\n");
-    printf("a:"); scanf("%d",&a);
-    printf("b:"); scanf("%d",&b);
+    printf("a:"); scanf("%u",&a);
+    printf("b:"); scanf("%u",&b);
 
     printf("\na     ="); print_bits(a);
     printf("\nb     ="); print_bits(b);
