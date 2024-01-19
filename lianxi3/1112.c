c1a4177c77b55d560ffe0f5c4b717bd8a2a4d4cd
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2024-01-19 20:13:41 +0800

    1112

---
+++ lianxi3/1112.c
@@ -0,0 +1,14 @@
+/*使用atoi函数的例子*/
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+    char str[128];
+
+    printf("请输入字符串:");
+    scanf("%s",str);
+
+    printf("转换后的整数为%d。\n",atoi(str));
+
+    return 0;
+}
