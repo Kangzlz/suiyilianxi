fdc71af6a10df4d95bd67595cced3497e956eee1
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2024-01-20 21:00:32 +0800

    1203

---
+++ lianxi3/1203.c
@@ -0,0 +1,20 @@
+/*用表示学生的结构体来显示高尾的信息*/
+#include<stdio.h>
+#define NAME_LEN 64
+struct student{
+    char name[NAME_LEN];
+    int height;
+    float weight;
+    long schols;
+};
+int main(void)
+{
+    struct student takao={"Takao",173,86.2};
+
+    printf("姓名=%s\n",takao.name);
+    printf("身高=%d\n",takao.height);
+    printf("体重=%.1f\n",takao.weight);
+    printf("奖学金=%ld\n",takao.schols);
+
+    return 0;
+}
