09fe1e0490547431240c4c77130275d74c522f30
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2024-01-26 23:52:49 +0800

    103

---
+++ mingjiezhongjipian/103.c
@@ -0,0 +1,25 @@
+/*猜数游戏【其二（另一种解法）：重复到猜对为止————利用while语句】*/
+#include<stdio.h>
+int main(void)
+{
+    int no;
+    int ans=7;
+
+    printf("请猜一个0~9的整数。\n\n");
+
+    while(1){
+        printf("是多少呢:");
+        scanf("%d",&no);
+
+        if(no>ans)
+        printf("\a再小一点。\n");
+        else if(no<ans)
+        printf("\a再大一点。\n");
+        else
+        break;
+    }
+
+    printf("回答正确。\n");
+
+    return 0;
+}
